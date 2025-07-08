474f8037981d7535220e99838332b9c995889da2
Alwaleed  Alharbi <waleedbdulaziz@gmail.com>
2025-07-08 10:27:18 +0300

    updating the schema fileds

--- src/models/DangerZone.model.ts
+++ src/models/DangerZone.model.ts
@@ -3,6 +3,10 @@ import mongoose from "mongoose";
 const dangerZoneSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: String,
+  location: {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+  },
 });
 
 export const DangerZone = mongoose.model("DangerZone", dangerZoneSchema);
