7374caf0d004064b08fac2ba1b14927d1241cd98
Alwaleed  Alharbi <waleedbdulaziz@gmail.com>
2025-07-08 10:18:54 +0300

    new danger zone creation feature

--- src/controllers/danger.controller.ts
+++ src/controllers/danger.controller.ts
@@ -1,2 +1,27 @@
 import { Request, Response } from "express";
 import { DangerZone } from "../models/DangerZone.model";
+
+// إضافة منطقة خطر جديدة
+export const reportDangerZone = async (
+  req: Request & { user?: any },
+  res: Response
+) => {
+  try {
+    const { name, description, location, city } = req.body;
+    const userId = req.user._id;
+
+    const newZone = await DangerZone.create({
+      name,
+      description,
+      location,
+      city,
+      reportedBy: userId,
+    });
+
+    res
+      .status(201)
+      .json({ message: "تمت إضافة منطقة الخطر بنجاح", dangerZone: newZone });
+  } catch (error) {
+    res.status(500).json({ message: "حدث خطأ أثناء إضافة منطقة الخطر" });
+  }
+};
